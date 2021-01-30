f8f447523ef60a6870f312406627e771e328964d
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2021-01-29 23:48:37 -0800

    Add random restart feature to get_tbd_url
    Using a random number generator, determine if the crawler should randomly select a link that is not the first link in the TBD link queue (last link of the list)

--- crawler/frontier.py
+++ crawler/frontier.py
@@ -7,6 +7,8 @@ from queue import Queue, Empty
 from utils import get_logger, get_urlhash, normalize
 from scraper import is_valid
 
+import random
+
 class Frontier(object):
     def __init__(self, config, restart):
         self.logger = get_logger("Frontier")
@@ -49,7 +51,22 @@ class Frontier(object):
 
     def get_tbd_url(self):
         try:
-            return self.to_be_downloaded.pop()
+            # TODO: Implement random restart.
+            # This part can be kept separate for logging purposes.
+            # I thought about implementing some sort of random shuffle
+            # instead of using an occasional random pop mid-list.
+            # For now, for complexity purposes, I'm sticking with
+            # using a random number generator to randomly select
+            # a link in the list but preserving the order. Hopefully,
+            # that link provides more links to add to the FIFO queue.
+            restart_threshold = 10
+            restart_value = random.randrange(0,100)
+            if (restart_value < restart_threshold):
+                random_restart = True
+                self.logger.info("Random restart triggered with <" + str(restart_threshold) + "% probability (" + str(restart_value) + "%)")
+            else:
+                random_restart = False
+            return self.to_be_downloaded.pop(random.randrange(0,len(self.to_be_downloaded)) if random_restart else -1)
         except IndexError:
             return None
 
